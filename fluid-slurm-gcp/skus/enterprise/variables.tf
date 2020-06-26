0ce49ed24bd890fa453fd71be4ba776e08001abb
Joe Schoonover <joe@fluidnumerics.com>
2020-06-26 17:59:35 -0600

    [FLU-1539] First draft of CloudSQL connected over private IP

--- fluid-slurm-gcp/skus/enterprise/variables.tf
+++ fluid-slurm-gcp/skus/enterprise/variables.tf
@@ -1,83 +1,97 @@
-variable "parent_folder" {
+variable "customer_folder" {
   type = string
   default = ""
   description = "A GCP folder id (folders/FOLDER-ID) that contains the Fluid-Slurm-GCP controller project and compute partition projects."
 }
 
-variable "slurm_gcp_admins" {
-  type = list(string)
-  description = "List of users or groups to provide slurm-gcp admin role"
-}
-
-variable "slurm_gcp_users" {
-  type = list(string)
-  description = "List of users or groups to provide slurm-gcp user role"
-}
-
-variable "slurm_gcp_name" {
+variable "customer_org_id" {
   type = string
+  description = "Customer organization ID from the managed-fluid-slurm-gcp customers database"
 }
 
-variable "slurm_gcp_tags" { 
-  type = list(string)
-  default = []
-}
-
-variable "vpc_network" {
+variable "cloud_sql_tier" {
   type = string
-  description = "Self link for the Shared VPC network hosting the cluster"
+  description = "Instance tier for the Cloud SQL instance that hosts the Slurm database"
+  default = "db-f1-micro"
 }
 
-variable "controller" { 
-  type = object({
-      machine_type = string
-      disk_size_gb = number
-      disk_type = string
-      labels = map(string)
-      project = string
-      region = string
-      vpc_subnet = string
-      zone = string
-  })
+variable "home_tier" {
+  type = string
+  description = "Filestore Tier for the home NFS server. Either STANDARD or PREMIUM"
+  default = "STANDARD"
+}
+
+variable "home_size_gb" {
+  type = number
+  description = "Size of the filestore home disk in GB. Minimum : 1024 for STANDARD and 2048 for PREMIUM."
+  default = 1024
+}
+
+variable "share_tier" {
+  type = string
+  description = "Filestore Tier for the share NFS server. Either STANDARD or PREMIUM"
+  default = "STANDARD"
+}
+
+variable "share_size_gb" {
+  type = number
+  description = "Size of the filestore share disk in GB. Minimum : 1024 for STANDARD and 2048 for PREMIUM. Set to 0 to disable the share filestore instance"
+  default = 0
+}
+variable "managing_domain" {
+  type = string
+  description = "The registered GSuite domain used to host Fluid-Slurm-GCP Cloud Identity Accounts"
+  default = "fluidnumerics.com"
+}
+
+variable "image_version" {
+  type = string
+  description = "Image version for the fluid-slurm-gcp images"
+  default = "v2-4-0"
+}
+
+variable "image_flavor" {
+  type = string
+  description = "Base Fluid-Slurm-GCP image flavor. One of `centos`, `ohpc`, or `ubuntu`"
+  default = "centos"
+}
+
+variable "primary_project" {
+  type = string
+  description = "Main GCP project ID for the customer's managed solution"
+}
+
+variable "primary_zone" {
+  type = string
+  description = "Main GCP zone for the customer's managed solution"
+}
+
+variable "whitelist_ssh_ips" {
+  type = list(string)
+  description = "Customer's IP addresses that should be added to a whitelist for ssh access"
+  default = ["0.0.0.0/0"]
+}
+
+variable "controller_machine_type" { 
+  type = string
+  description = "GCP Machine type to use for the login node."
 }
 
 variable "default_partition" {
   type = string
   description = "Name of the default compute partition."
-  default = "default"
+  default = ""
 }
 
-variable "login" {
-  type= list(object({
-      machine_type = string
-      disk_size_gb = number
-      disk_type = string
-      labels = map(string)
-      project = string
-      region = string
-      vpc_subnet = string
-      zone = string
-  }))
-}
-
-variable "mounts" {
-  type = list(object({
-      group = string
-      mount_directory = string
-      mount_options = string
-      owner = string
-      protocol = string
-      permission = string
-      server_directory = string
-  }))
-  default = []
+variable "login_machine_type" {
+  type = string
+  description = "GCP Machine type to use for the login node."
 }
 
 variable "partitions" {
   type = list(object({
       name = string
       project = string
-      vpc_subnet= string
       max_time= string
       labels = map(string)
       machines = list(object({
@@ -88,6 +102,7 @@ variable "partitions" {
         external_ip = bool
         gpu_count = number
         gpu_type = string
+        image = string
         n_local_ssds = number
         local_ssd_mount_directory = string
         machine_type=string
@@ -95,9 +110,12 @@ variable "partitions" {
         preemptible_bursting= bool
         static_node_count= number
         vpc_subnet = string
-        zone= string    
+        zone= string
       }))
   }))
+  description = "Settings for partitions and compute instances available to the cluster."
+  
+  default = []
 }
 
 variable "slurm_accounts" {
@@ -117,24 +135,4 @@ variable "munge_key" {
 variable "suspend_time" {
   type = number
   default = 300
-}
-
-variable "home_capacity_gb" {
-  type = number
-  default = 2048
-}
-
-variable "home_tier" {
-  type = string
-  default = "STANDARD"
-}
-
-variable "share_capacity_gb" {
-  type = number
-  default = 0
-}
-
-variable "share_tier" {
-  type = string
-  default = "STANDARD"
 }
